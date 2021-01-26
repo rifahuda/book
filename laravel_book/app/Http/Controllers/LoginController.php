6fdbdd09d33b3a6d9969ed1cb844c510179853d3
aidensV <nameblacllis@gmail.com>
2021-01-26 10:27:17 +0700

    username login

--- laravel_book/app/Http/Controllers/LoginController.php
+++ laravel_book/app/Http/Controllers/LoginController.php
@@ -2,98 +2,101 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use App\Models\User;
-use DB;
 
-class LoginController extends Controller
-{
-    /**
-     * Handle an authentication attempt.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
-    public function authenticate(Request $request)
-    {
-        // dd($request->all());
-        // $credentials = $request->only('email', 'password');
-        // dd(Auth::attempt(['u_email' => $request->username, 'password' => $request->password]));
-        // dd(Hash::check('123456','$2y$10$hTe.5kpNrDqutNGGL3hEDOs0J2RcUG7DAz1EL4THyh7lUaD4wj0gO'));
-        
-        $user = User::where('u_email',$request->username)->get();
-        // dd($user);
-        foreach ($user as $key => $value) {
-            $userPass = Hash::check($request->password,$value->u_password);
-            if($userPass){
-                
-                $request->session()->regenerate();    
-                $request->session()->put('user', User::where('id',$value->id)->first());
-                return redirect()->intended('dashboard');
-            }
-            // if (Auth::attempt(['u_email' => $request->username, 'password' => $request->password])) {
-            //     return redirect()->intended('dashboard');
-            //     $request->session()->regenerate();
-            // }
-            
-        }
-        
+class LoginController extends Controller {
+	/**
+	 * Handle an authentication attempt.
+	 *
+	 * @param  \Illuminate\Http\Request $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function authenticate(Request $request) {
+		// dd($request->all());
+		// $credentials = $request->only('email', 'password');
+		// dd(Auth::attempt(['u_email' => $request->username, 'password' => $request->password]));
+		// dd(Hash::check('123456','$2y$10$hTe.5kpNrDqutNGGL3hEDOs0J2RcUG7DAz1EL4THyh7lUaD4wj0gO'));
+		// dd(!filter_var($request->username, FILTER_VALIDATE_EMAIL));
+		if (!filter_var($request->username, FILTER_VALIDATE_EMAIL)) {
+			// $usename = $request->username;
 
-        return back()->withErrors([
-            'email' => 'The provided credentials do not match our records.',
-        ]);
-    }
+			$user = User::where('u_username', $request->username)->get();
+		} else {
+			// $usename = $request->username;
+			$user = User::where('u_email', $request->username)->get();
 
-    public function cekakun(Request $request){
-        $userId = $request->session()->get('user')->u_email;
-        $user = User::where('u_email',$userId)->count();
-        $statusAkun = false;
-        if($user > 0){
-            $statusAkun = true;
-        }
-        return response()->json([
-            'status' => 'success',
-            'akun' => $statusAkun
-        ]);
-    }
+		}
 
-    
+		foreach ($user as $key => $value) {
+			$userPass = Hash::check($request->password, $value->u_password);
 
-        /**
-     * Handle an authentication attempt.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $userId = $request->session()->get('user')->u_email;
-        DB::Begintransaction();
+			if ($userPass) {
 
-        try {
-            // $anggotaId = Auth::user()->u_anggota_id;
-            User::create([
-                'u_email' => $request->email,
-                'u_anggota_id' => $userId->u_anggota_id,
-                'u_username' => $request->username,
-                'u_password' =>  Hash::make($request->password),
-            ]);
-            DB::Commit();
-            return response()->json([
-                'status' => 'success',
-                'message' => 'Berhasil '
-            ]);
-        } catch (\Throwable $th) {
-            DB::rollback();
-            return $th;           
-        }
-    }
+				$request->session()->regenerate();
+				$request->session()->put('user', User::where('id', $value->id)->first());
+				return redirect()->intended('dashboard');
+			}
+			// if (Auth::attempt(['u_email' => $request->username, 'password' => $request->password])) {
+			//     return redirect()->intended('dashboard');
+			//     $request->session()->regenerate();
+			// }
 
-    public function logout(Request $request)
-    {
-        $request->session()->forget('user');
-        return view('auth.login');
-    }
+		}
+
+		return back()->withErrors([
+			'email' => 'The provided credentials do not match our records.',
+		]);
+	}
+
+	public function cekakun(Request $request) {
+		$userId = $request->session()->get('user')->u_email;
+		$user = User::where('u_email', $userId)->count();
+		$statusAkun = false;
+		if ($user > 0) {
+			$statusAkun = true;
+		}
+		return response()->json([
+			'status' => 'success',
+			'akun' => $statusAkun,
+		]);
+	}
+
+	/**
+	 * Handle an authentication attempt.
+	 *
+	 * @param  \Illuminate\Http\Request $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request) {
+		$userId = $request->session()->get('user')->u_anggota_id;
+
+		DB::Begintransaction();
+
+		try {
+			// $anggotaId = Auth::user()->u_anggota_id;
+			User::create([
+				'u_email' => $request->email,
+				'u_anggota_id' => $userId,
+				'u_username' => $request->username,
+				'u_password' => Hash::make($request->password),
+			]);
+			DB::Commit();
+			return response()->json([
+				'status' => 'success',
+				'message' => 'Berhasil ',
+			]);
+		} catch (\Throwable $th) {
+			DB::rollback();
+			return $th;
+		}
+	}
+
+	public function logout(Request $request) {
+		$request->session()->forget('user');
+		return redirect('/');
+	}
 }
